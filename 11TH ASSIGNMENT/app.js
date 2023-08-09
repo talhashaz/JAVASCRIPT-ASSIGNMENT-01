09813f925748206ff8acca3bd989eed91cde9340
talhashaz <135649557+talhashaz@users.noreply.github.com>
2023-08-09 21:24:26 +0500

    Add files via upload

---
+++ 11TH ASSIGNMENT/app.js
@@ -0,0 +1,53 @@
+/* PDF CHAPTER 1
+
+alert("Error! Please Enter a valid password")
+
+
+alert("Welcome to Jsland....\nHappy Coding")
+
+
+alert("Welcome to JS land")
+
+
+alert("Happy coding!")
+
+
+alert("Hello...I can run JS through my web browser's console")
+*/
+
+
+
+
+/* PDF CHAPTER 2
+alert("Jhon doe")
+
+
+alert("15 year old")
+
+
+alert("Certified Mobile Application Development")
+
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+
+alert("My email address is example@example.com")
+
+
+alert("I am trying to learn from the book A smarter way to learn JavaScript")
+*/
+
+
+
+
+
+/*PDF CHAPTER 3
+alert("I am 15 year old")
+
+
+alert("You have visited this site 14 times")
+*/
+
+
+
+
